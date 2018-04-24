0cfc7bd8390e0d54344010cf2cd0d5a40125f6eb
lyric <tiannianshou@gmail.com>
2018-04-24 23:02:08 +0800

    update doc

--- doc.go
+++ doc.go
@@ -3,56 +3,56 @@
 /*
 Example:
 
-		package main
+	package main
 
-		import (
-			"context"
-			"fmt"
-			"net/http"
+	import (
+		"context"
+		"fmt"
+		"net/http"
 
-			"gopkg.in/session.v2"
+		"gopkg.in/session.v2"
+	)
+
+	func main() {
+		session.InitManager(
+			session.SetCookieName("session_id"),
+			session.SetSign([]byte("sign")),
 		)
 
-		func main() {
-			session.InitManager(
-				session.SetCookieName("session_id"),
-				session.SetSign([]byte("sign")),
-			)
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			store, err := session.Start(context.Background(), w, r)
+			if err != nil {
+				fmt.Fprint(w, err)
+				return
+			}
 
-			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				store, err := session.Start(context.Background(), w, r)
-				if err != nil {
-					fmt.Fprint(w, err)
-					return
-				}
+			store.Set("foo", "bar")
+			err = store.Save()
+			if err != nil {
+				fmt.Fprint(w, err)
+				return
+			}
 
-				store.Set("foo", "bar")
-				err = store.Save()
-				if err != nil {
-					fmt.Fprint(w, err)
-					return
-				}
+			http.Redirect(w, r, "/foo", 302)
+		})
 
-				http.Redirect(w, r, "/foo", 302)
-			})
+		http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+			store, err := session.Start(context.Background(), w, r)
+			if err != nil {
+				fmt.Fprint(w, err)
+				return
+			}
 
-			http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-				store, err := session.Start(context.Background(), w, r)
-				if err != nil {
-					fmt.Fprint(w, err)
-					return
-				}
+			foo, ok := store.Get("foo")
+			if ok {
+				fmt.Fprintf(w, "foo:%s", foo)
+				return
+			}
+			fmt.Fprint(w, "does not exist")
+		})
 
-				foo, ok := store.Get("foo")
-				if ok {
-					fmt.Fprintf(w, "foo:%s", foo)
-					return
-				}
-				fmt.Fprint(w, "does not exist")
-			})
-
-			http.ListenAndServe(":8080", nil)
-		}
+		http.ListenAndServe(":8080", nil)
+	}
 
 Learn more at https://github.com/go-session/session
 */
